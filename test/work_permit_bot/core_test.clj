498d535fc78ec70eeb9ada7bbc57386be367a75c
Francisca <fransanalves@outlook.com>
2020-08-16 21:29:44 -0300

    initial commit

---
+++ test/work_permit_bot/core_test.clj
@@ -0,0 +1,7 @@
+(ns work-permit-bot.core-test
+  (:require [clojure.test :refer :all]
+            [work-permit-bot.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
